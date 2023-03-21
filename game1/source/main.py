25dc1b06d4c13a445b045c2188d7f9f997742de4
Xenon Zenon <jayson_abad@outlook.com>
2023-03-21 15:26:14 +0800

    0.0.0

--- game1/source/main.py
+++ game1/source/main.py
@@ -8,9 +8,64 @@ class Game:
         pygame.display.set_caption("Game 1")
         self.win = pygame.display.set_mode((500, 500))
         self.running = True
+        self.x_pos = 0
+        self.y_pos = 0
+        self.rec_width = 0
+        self.rec_height = 0
+        self.velocity = 0
 
-    def run(self):
+    def character(self):
+        self.x_pos = 50
+        self.y_pos = 50
+        self.rec_width = 40
+        self.rec_height = 60
+        self.velocity = 10
+
+    def setvar(self):
+        self.character()
+
+    def keyevents(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x_pos -= self.velocity
+
+        if keys[pygame.K_RIGHT]:
+            self.x_pos += self.velocity
+
+        if keys[pygame.K_UP]:
+            self.y_pos -= self.velocity
+
+        if keys[pygame.K_DOWN]:
+            self.y_pos += self.velocity
+
+    def draw(self):
+        self.win.fill((0, 0, 0))
+        pygame.draw.rect(
+            self.win,
+            (255, 0, 0),
+            (
+                self.x_pos,
+                self.y_pos,
+                self.rec_width,
+                self.rec_height
+            )
+        )
+        pygame.display.update()
+
+    def mainloop(self):
         while self.running:
+            pygame.time.delay(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+
+            self.keyevents()
+            self.draw()
+
+    def run(self):
+        self.setvar()
+        self.mainloop()
+        self.close()
+
+    def close(self):
+        pygame.quit()
--- game1/takbo.py
+++ game1/takbo.py
@@ -1,6 +1,5 @@
 from source.main import Game
 
 if __name__ == '__main__':
-
     game = Game()
     game.run()
