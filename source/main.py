0f12fad6c4307cbbcbbc1931eeada08fa566ad54
Xenon Zenon <jayson_abad@outlook.com>
2023-03-21 14:52:17 +0800

    initial commit

---
+++ source/main.py
@@ -0,0 +1,16 @@
+import pygame
+
+
+class Game:
+
+    def __init__(self):
+
+        self.test()
+
+    def test(self):
+
+        print("Test...")
+
+    def run(self):
+
+        print("Running...")
