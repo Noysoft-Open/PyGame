9e4961b7417a8d27e09e9a387ed9e2290855fc77
Xenon Zenon <jayson_abad@outlook.com>
2023-03-21 15:04:07 +0800

    0.0.0

--- game1/source/main.py
+++ game1/source/main.py
@@ -4,13 +4,13 @@ import pygame
 class Game:
 
     def __init__(self):
-
-        self.test()
-
-    def test(self):
-
-        print("Test...")
+        pygame.init()
+        pygame.display.set_caption("Game 1")
+        self.win = pygame.display.set_mode((500, 500))
+        self.running = True
 
     def run(self):
-
-        print("Running...")
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
